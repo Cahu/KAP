cb331a153bdb528a8e5ec0dc62bc27f712b84012
cahu <cahu@gmx.us>
2016-09-10 20:08:09 +0200

    suborbital tourism

---
+++ app/3_RadialSRBs.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Lib
+
+import KRPCHS
+import KRPCHS.SpaceCenter
+
+import Control.Monad
+import Control.Monad.Trans
+
+
+{- Slight modification of the previous design to allow transporting 4 kerbals
+ - for suborbital tourism contracts. This uses two small radial SRBs (in
+ - addition to the main SR motor) which are jettisoned when a sudden drop in
+ - thrust is detected.
+ -
+ - Again, no control on this one. The rocket uses fins and TWR tweaking.
+ -}
+
+main :: IO ()
+main =
+    withRPCClient    "3_RadialSRBs" "127.0.0.1" "50000" $ \client ->
+    withStreamClient client         "127.0.0.1" "50001" $ \streamClient ->
+        runRPCProg client (mainProg streamClient)
+
+
+mainProg :: StreamClient -> RPCContext ()
+mainProg streamClient =
+    getStandardVesselPackage >>= \StandardVesselPackage{..} ->
+
+    let stage = void $ controlActivateNextStage control
+
+        -- wait for a sudden drop in thrust
+        waitBoosterBurnout thrustStream = keepTryingOnExcept NoSuchStream $
+            monitorStreamChange streamClient thrustStream (\oldval newval -> oldval - newval > 100)
+
+        waitStartFalling vSpeedStream = keepTryingOnExcept NoSuchStream $
+            monitorStreamWait streamClient vSpeedStream (< (-10))
+
+        waitSafeAltitude altStream = keepTryingOnExcept NoSuchStream $
+            monitorStreamWait streamClient altStream (< (7000))
+
+    in do
+        stage -- launch
+        withStream (getVesselAvailableThrustStreamReq vessel) waitBoosterBurnout
+        stage -- decouble boosters
+        withStream (getFlightVerticalSpeedStreamReq   flight) waitStartFalling
+        stage -- decouple pod
+        withStream (getFlightSurfaceAltitudeStreamReq flight) waitSafeAltitude
+        stage -- activate chutes
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,8 @@ module Lib
 , getStandardVesselPackage
 , keepTryingOnExcept
 , monitorStreamWait
+, monitorStreamChange
+, displayStreamValues
 ) where
 
 
@@ -13,6 +15,7 @@ import KRPCHS.SpaceCenter
 
 import Control.Monad
 import Control.Monad.Catch
+import Control.Monad.Trans
 
 
 data StandardVesselPackage = StandardVesselPackage
@@ -53,3 +56,24 @@ monitorStreamWait client stream predicate = loop
             msg <- getStreamMessage client
             val <- getStreamResult stream msg
             unless (predicate val) loop
+
+
+monitorStreamChange :: KRPCResponseExtractable a => StreamClient -> KRPCStream a -> (a -> a -> Bool) -> RPCContext ()
+monitorStreamChange client stream predicate = loop Nothing
+    where
+        loop maybeVal = do
+            msg <- getStreamMessage client
+            val <- getStreamResult stream msg
+            case maybeVal of
+                Nothing     -> loop (Just val)
+                Just oldVal -> unless (predicate oldVal val) (loop $ Just val)
+
+
+displayStreamValues :: (Show a, KRPCResponseExtractable a) => StreamClient -> KRPCStream a -> RPCContext ()
+displayStreamValues client stream = loop
+    where
+        loop = do
+            msg <- getStreamMessage client
+            val <- getStreamResult stream msg
+            liftIO $ print val
+            loop
